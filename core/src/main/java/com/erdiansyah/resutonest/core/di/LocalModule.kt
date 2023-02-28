cd1b5dbbe0cffac2cef044e2ae5408cf9cc9d6ab
erdiansyah-me <erdiansyah465@gmail.com>
2023-02-28 14:46:07 +0700

    add some security function

--- core/src/main/java/com/erdiansyah/resutonest/core/di/LocalModule.kt
+++ core/src/main/java/com/erdiansyah/resutonest/core/di/LocalModule.kt
@@ -2,6 +2,7 @@ package com.erdiansyah.resutonest.core.di
 
 import android.content.Context
 import androidx.room.Room
+import com.erdiansyah.resutonest.core.BuildConfig
 import com.erdiansyah.resutonest.core.data.source.local.db.FavoriteDao
 import com.erdiansyah.resutonest.core.data.source.local.db.FavoriteDb
 import dagger.Module
@@ -9,6 +10,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -16,10 +19,14 @@ class LocalModule {
     @Provides
     fun provideDatabase(
         @ApplicationContext context: Context) : FavoriteDb {
+        val passPhrase: ByteArray = SQLiteDatabase.getBytes(BuildConfig.PASSBASE.toCharArray())
+        val factory = SupportFactory(passPhrase)
         return Room.databaseBuilder(
             context.applicationContext,
             FavoriteDb::class.java, "favorite.db"
-        ).build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 
     @Provides
--- core/src/main/java/com/erdiansyah/resutonest/core/di/NetworkModule.kt
+++ core/src/main/java/com/erdiansyah/resutonest/core/di/NetworkModule.kt
@@ -6,6 +6,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -17,10 +18,16 @@ import java.util.concurrent.TimeUnit
 class NetworkModule {
     @Provides
     fun provideOkHttpClient(): OkHttpClient {
+        val certificatePinner = CertificatePinner.Builder()
+            .add(BuildConfig.BASE_URL_PATTERN, BuildConfig.BASE_URL_SHA256_1)
+            .add(BuildConfig.BASE_URL_PATTERN, BuildConfig.BASE_URL_SHA256_2)
+            .add(BuildConfig.BASE_URL_PATTERN, BuildConfig.BASE_URL_SHA256_3)
+            .build()
         return OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
 
